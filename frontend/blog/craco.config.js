d5fa900749bcdc27c3fe73c44bdb2f119037d47a
taeukHa <hive0917@gmail.com>
2023-07-31 05:16:10 +0900

    feat([front]blog-service): Add Router
    Root route setting and error page handling

--- frontend/blog/craco.config.js
+++ frontend/blog/craco.config.js
@@ -1,16 +1,21 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    babel: {
-        presets: ['@emotion/babel-preset-css-prop'],
-    },
-    webpack: {
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@utils': path.resolve(__dirname, 'src/utils'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@styles': path.resolve(__dirname, 'src/styles'),
-        },
-    },
+    plugin: [
+        {
+            plugin: path,
+            options: {
+                source: 'options',
+                baseUrl: './',
+                alias: {
+                    '@components': path.resolve(__dirname, 'src/components'),
+                    '@utils': path.resolve(__dirname, 'src/utils'),
+                    '@assets': path.resolve(__dirname, 'src/assets'),
+                    '@pages': path.resolve(__dirname, 'src/pages'),
+                    '@styles': path.resolve(__dirname, 'src/styles'),
+                    '@routes': path.resolve(__dirname, 'src/routes'),
+                },
+            }
+        }
+    ]
 };
--- frontend/blog/src/index.js
+++ frontend/blog/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootRoutes from "./RootRoutes";
+import ErrorPage from "./errorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootRoutes />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
