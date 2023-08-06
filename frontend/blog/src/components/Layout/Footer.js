1d4dd46f6bde60eef38e1ee4df789c7320d0037c
taeukHa <hive0917@gmail.com>
2023-08-06 23:59:25 +0900

    Update([front]blog-service): Layout Components
    1. Change mui AppBar interface
    2. Apply Footer

--- frontend/blog/src/components/Layout/Footer.js
+++ frontend/blog/src/components/Layout/Footer.js
@@ -1,10 +1,30 @@
-import Header from "./Header";
-import {Outlet} from "react-router-dom";
+import {Box, Container, Link, Typography} from "@mui/material";
 
 export default function Footer() {
+
     return (
         <>
-            <h1>Footer</h1>
+            <Box
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === "light"
+                            ? theme.palette.grey[200]
+                            : theme.palette.grey[800],
+                    p: 6,
+                }}
+                component="footer"
+            >
+                <Container maxWidth="sm">
+                    <Typography variant="body2" color="text.secondary" align="center">
+                        {"Copyright © "}
+                        <Link color="inherit" href="https://www.hive.com/">
+                            https://www.hive.com/
+                        </Link>{" "}
+                        {new Date().getFullYear()}
+                        {"."}
+                    </Typography>
+                </Container>
+            </Box>
         </>
     )
 };
--- frontend/blog/src/components/Layout/Header.js
+++ frontend/blog/src/components/Layout/Header.js
@@ -2,99 +2,54 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import {
     AppBar,
-    Box, Button, CssBaseline,
-    Divider,
-    Drawer,
-    IconButton, Link,
-    List,
-    ListItem, ListItemButton,
-    ListItemText,
+    Box, Button, Container, CssBaseline,
+    Link,
     Toolbar,
-    Typography
+    Typography, useScrollTrigger
 } from "@mui/material";
 
-const drawerWidth = 240;
-const navItems = ['Intro', 'Blog', 'Contact'];
+function ElevationScroll(props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
 
-function Header(props) {
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
-    };
-
-    /*const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-            <Typography variant="h6" sx={{ my: 2 }}>
-                하태욱1
-            </Typography>
-            <Divider />
-            <List>
-                {navItems.map((item) => (
-                    <ListItem key={item} disablePadding>
-                        <ListItemButton sx={{ textAlign: 'center' }}>
-                            <ListItemText primary={item} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );*/
-    const container = window !== undefined ? () => window().document.body : undefined;
-
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="static" component="nav" sx={{ bgcolor: "Black", height: '70px' }}>
-                <Toolbar color="Black">
-                    <Typography
-                        variant="h5"
-                        component="div"
-                        sx={{flexGrow: 0.9, display: { xs: 'none', sm: 'block' }, fontWeight: "bold"}}
-                    >
-                        <Link href="/blog/tech" underline="none" color="White" sx={{ml:10}}>Hive</Link>
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        {navItems.map((item) => (
-                            <Button key={item} sx={{ color: '#fff', fontSize: 16 }}>
-                                {item}
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-
-
-           {/* <Box component="nav">
-                <Drawer
-                    container={container}
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: { xs: 'block', sm: 'none' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-            </Box>
-            <Box component="main" sx={{ p: 3 }}>
-                <Toolbar />
-                <Typography>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-                </Typography>
-            </Box>*/}
-        </Box>
-    );
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
 }
 
-Header.propTypes = {
+ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
     window: PropTypes.func,
 };
 
-export default Header;
+export default function Header(props) {
+    const navItems = ['Intro', 'Blog', 'Contact', 'Memoirs'];
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <ElevationScroll {...props}>
+                <AppBar id="page-header" sx={{ bgcolor: "Black" }}>
+                    <Toolbar>
+                        <Typography variant="h4" component="div"
+                                    sx={{flexGrow: 0.9, display: { xs: 'none', sm: 'block' }, fontWeight: "bold"}}>
+                            <Link href="/blog/tech" underline="none" color="White" sx={{ml:10}} >Hive</Link>
+                        </Typography>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            {navItems.map((item) => (
+                                <Button key={item} variant="h5" sx={{ color: '#fff', fontSize: 16 }}>
+                                 {item}
+                                </Button>
+                         ))}
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+        </React.Fragment>
+    )
+}
